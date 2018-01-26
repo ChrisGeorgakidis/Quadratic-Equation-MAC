e9074d5d3fa4e4172304ec94522647139439a347
giannisst <yanni.stamoulis@gmail.com>
2018-01-26 19:05:07 +0200

    Mode0 implementation + tb

--- Quadratic_Equation.v
+++ Quadratic_Equation.v
@@ -1,3 +1,27 @@
-module Quadratic_Equation ();
+module Quadratic_Equation (clk, reset, in_a, in_x, in_b, in_c, result);
 
+input wire clk, reset;
+input wire [7:0]in_a;
+input wire [7:0]in_x;
+input wire [7:0]in_b;
+input wire [7:0]in_c;
+output reg [15:0]result;
+
+reg [15:0]R;
+reg [7:0]C;
+reg [7:0]rX;
+
+always @ ( posedge clk or posedge reset ) begin
+    if (reset == 1) begin
+        R <= 16'b0;
+        C <= 8'b0;
+        result <= 16'b0;
+    end
+    else begin
+        rX <= in_x;
+        C <= in_c;
+        R <= (in_a * in_x) + in_b;
+        result <= (R * rX) + C;
+    end
+end
 endmodule // Quadratic_Equation
---
+++ mode0_tb.v
@@ -0,0 +1,42 @@
+module mode0_tb ();
+
+reg clk, reset;
+reg [7:0]in_a;
+reg [7:0]in_x;
+reg [7:0]in_b;
+reg [7:0]in_c;
+wire [15:0]result;
+
+Quadratic_Equation Quadratic_Equation(
+    .clk    (clk),
+    .reset  (reset),
+    .in_a   (in_a),
+    .in_x   (in_x),
+    .in_b   (in_b),
+    .in_c   (in_c),
+    .result (result)
+);
+
+initial begin
+    clk <= 1;
+    reset <= 1;
+    #50reset <= 0;
+    in_a <= 8'd5;
+    in_x <= 8'd8;
+    in_b <= 8'd13;
+    in_c <= 8'd7;
+    #10in_a <= 8'd1;
+    in_x <= 8'd0;
+    in_b <= 8'd2;
+    in_c <= 8'd3;
+    #10in_a <= 8'd25;
+    in_x <= 8'd4;
+    in_b <= 8'd18;
+    in_c <= 8'd10;
+end
+
+always begin
+    #5clk <= ~clk;
+end
+
+endmodule
